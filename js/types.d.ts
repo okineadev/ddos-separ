dc9e8264df3b46048c714a4cd9be008ba61082b6
Bogdan <81070564+BogdanDevUA@users.noreply.github.com>
2022-12-22 12:49:34 +0200

    Signed-off-by: Bogdan <81070564+BogdanDevUA@users.noreply.github.com>
    fix

--- js/types.d.ts
+++ js/types.d.ts
@@ -2,91 +2,88 @@
 
 /** Батарея */
 declare interface BatteryManager {
-	/**
-	 * Статус зарядки
-	 *
-	 * @example await navigator.getBattery().charging // true
-	 */
-	charging: boolean;
+    /**
+     * Статус зарядки
+     *
+     * @example await navigator.getBattery().charging // true
+     */
+    charging: boolean;
 
-	/**
-	 * Час, що залишився до повної зарядки
-	 *
-	 * @example await navigator.getBattery().chargingTime // 120
-	 */
-	chargingTime: number;
+    /**
+     * Час, що залишився до повної зарядки
+     *
+     * @example await navigator.getBattery().chargingTime // 120
+     */
+    chargingTime: number;
 
-	/**
-	 * Час, що залишився до повної розрядки
-	 *
-	 * @example await navigator.getBattery().dischargingTime // 120
-	 */
-	dischargingTime: number;
+    /**
+     * Час, що залишився до повної розрядки
+     *
+     * @example await navigator.getBattery().dischargingTime // 120
+     */
+    dischargingTime: number;
 
-	/**
-	 * Рівень заряду батареї
-	 *
-	 * @example await navigator.getBattery().level * 100 // 87
-	 */
-	level: number;
+    /**
+     * Рівень заряду батареї
+     *
+     * @example await navigator.getBattery().level * 100 // 87
+     */
+    level: number;
 
-	/**
-	 * Коли було підключено/відключено зарядний пристрій
-	 *
-	 * @example await navigator.getBattery().onchargingchange = function () {console.log(this)} // BatteryManager {...}
-	 */
-	onchargingchange: null;
+    /**
+     * Коли було підключено/відключено зарядний пристрій
+     *
+     * @example await navigator.getBattery().onchargingchange = function () {console.log(this)} // BatteryManager {...}
+     */
+    onchargingchange: null;
 
-	/**
-	 * Коли змінився час до повної зарядки
-	 *
-	 * @example await navigator.getBattery().onchargingtimechange = function () {console.log(this)} // BatteryManager {...}
-	 */
-	onchargingtimechange: null;
+    /**
+     * Коли змінився час до повної зарядки
+     *
+     * @example await navigator.getBattery().onchargingtimechange = function () {console.log(this)} // BatteryManager {...}
+     */
+    onchargingtimechange: null;
 
-	/**
-	 * Коли змінився час до повної розрядки
-	 *
-	 * @example await navigator.getBattery().ondischargingtimechange = function () {console.log(this)} // BatteryManager {...}
-	 */
-	ondischargingtimechange: null;
+    /**
+     * Коли змінився час до повної розрядки
+     *
+     * @example await navigator.getBattery().ondischargingtimechange = function () {console.log(this)} // BatteryManager {...}
+     */
+    ondischargingtimechange: null;
 
-	/**
-	 * Коли змінився рівень заряду
-	 *
-	 * @example await navigator.getBattery().onlevelchange = function () {console.log(this)} // BatteryManager {...}
-	 */
-	onlevelchange: null;
+    /**
+     * Коли змінився рівень заряду
+     *
+     * @example await navigator.getBattery().onlevelchange = function () {console.log(this)} // BatteryManager {...}
+     */
+    onlevelchange: null;
 }
 
 declare interface Navigator {
-	/**
-	 * Отримати данні про батарею
-	 *
-	 * @example await navigator.getBattery() // BatteryManager {...}
-	 */
-	getBattery(): Promise<BatteryManager>;
+    /**
+     * Отримати данні про батарею
+     *
+     * @example await navigator.getBattery() // BatteryManager {...}
+     */
+    getBattery(): Promise<BatteryManager>;
 
-	/**
-	 * Отримати данні про батарею
-	 *
-	 * @example navigator.battery // BatteryManager {...}
-	 */
-	battery: BatteryManager;
+    /**
+     * Отримати данні про батарею
+     *
+     * @example navigator.battery // BatteryManager {...}
+     */
+    battery: BatteryManager;
 }
 
 /** Ціль для атаки */
 declare interface Target {
-	/**
-	 * Сторінка для атаки
-	 *
-	 * @example `https://russia.ru`
-	 */
-	page: string;
+    /**
+     * Сторінка для атаки
+     *
+     * @example `https://russia.ru`
+     */
+    page: string;
 
-	/** Метод для атаки */
-	method: "GET" | "POST";
+    /** Метод для атаки */
+    method: "GET" | "POST";
 }
-
-declare var TargetsLoadingError = Error("олів");
-declare var TargetsEncodingError = Error("лдтол");
