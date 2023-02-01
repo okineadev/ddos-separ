f2c6f98dd1722690b557a78b1512f34f6f6b607f
Bogdan <81070564+BogdanDevUA@users.noreply.github.com>
2023-02-01 21:56:53 +0200

    ik

--- gulp-tasks/build_html.js
+++ gulp-tasks/build_html.js
@@ -4,5 +4,5 @@ const htmlmin = require('gulp-htmlmin');
 module.exports = function build_html() {
     return src('index.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(dest('build'))
+        .pipe(dest('build/'))
 }
